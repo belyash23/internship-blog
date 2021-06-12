ef2be2fa64f7fc7fa1e40552174cfa301653423e
Anton Belyaev <belyash23@inbox.ru>
2021-06-12 11:14:48 +0400

    create database

--- config/db.php
+++ config/db.php
@@ -2,10 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'sqlite:@app/blog.db',
+    'tablePrefix' => 'tbl_',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
